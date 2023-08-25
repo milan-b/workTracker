1a5299a3474b657abf9bd8c81963ee4c17386460
milan.bojic <bojic.job@gmail.com>
2023-08-26 00:12:20 +0200

    added newtonsoft.json for time zone issue

--- WorkTracker/WebAPI/Program.cs
+++ WorkTracker/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.HttpOverrides;
+using Newtonsoft.Json;
 using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.ConfigureDBContext(builder.Configuration);
 builder.Services.ConfigureRepositoryWrapper();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(
+       options => options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
