91d2e7ccebd8117e915eef11e44e0d21ab12eeeb
milan.bojic <bojic.job@gmail.com>
2023-08-30 00:42:18 +0200

    change from sass to scss

--- Client/src/app/shared/notifications/error-notification/error-notification.component.ts
+++ Client/src/app/shared/notifications/error-notification/error-notification.component.ts
@@ -4,7 +4,7 @@ import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-error-notification',
   templateUrl: './error-notification.component.html',
-  styleUrls: ['./error-notification.component.sass']
+  styleUrls: ['./error-notification.component.scss']
 })
 export class ErrorNotificationComponent {
   constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) { }
--- Client/src/app/shared/notifications/info-notification/info-notification.component.ts
+++ Client/src/app/shared/notifications/info-notification/info-notification.component.ts
@@ -4,7 +4,7 @@ import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-info-notification',
   templateUrl: './info-notification.component.html',
-  styleUrls: ['./info-notification.component.sass']
+  styleUrls: ['./info-notification.component.scss']
 })
 export class InfoNotificationComponent {
 
