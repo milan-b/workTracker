256bb2c4ed8ed57b9477a531a5fb21d24e424409
Milan Bojic <bojic.job@gmail.com>
2024-01-12 22:20:47 +0100

    person create/edit actions

--- Client/src/app/person/list/list.component.ts
+++ Client/src/app/person/list/list.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ListDataSource } from './list-datasource';
 import { Person } from '../person.model';
 import { PersonService } from '../person.service';
+import * as routs from 'src/app/routs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -17,14 +18,22 @@ export class ListComponent implements AfterViewInit {
   dataSource = new ListDataSource(this.personService);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'name', 'actions'];
 
-  constructor(private personService: PersonService){
+  constructor(private personService: PersonService, private router: Router){
 
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.table.dataSource = this.dataSource;
+  }
+
+  goToEdit(id: number) {
+    this.router.navigate([routs.PERSON + '/' + routs.EDIT_ID + id]);
+  }
+
+  goToCreate() {
+    this.router.navigate([routs.PERSON + '/' + routs.CREATE]);
   }
 }
